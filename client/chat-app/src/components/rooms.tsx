823747614c4d986484fa896619da9626d3530c60
henrietta jeansson <henriettajeansson@gmail.com>
2023-08-28 14:04:27 +0200

    room component skeleton

--- client/chat-app/src/components/rooms.tsx
+++ client/chat-app/src/components/rooms.tsx
@@ -1,22 +1,50 @@
 
 
-//Detta kommer att ligga som en "sidebar"
+//Detta kommer att ligga som en "sidebar" och ska bara visas när användaren
+// valt sitt username
 
-export function Rooms({ }: type) => {
-
-
-
-    const chatSetup = () => {
-
-        //map. meddelanden
+import { useRef } from "react";
+import { useSockets } from "../context/socketContext";
 
 
-        <section>
-            <p>
-            </p>
+function Rooms() {
+
+    const { socket, roomId, rooms } = useSockets();
+    const createRoom = useRef(null);
+
+
+    //hantera skapandet av rum
+
+    function handleCreateRoom() {
+
+        //hämta namnet på rum
+
+        //emitta skapat rum
+
+        //inputtaggen som tom string
+
+    }
+
+
+
+    const roomSetup = () => {
+        <section className="room-container">
+            <input ref={createRoom} placeholder=" Call your room something... " />
+            <button onClick={handleCreateRoom}>Create your room!</button>
         </section>
+        //map. meddelande-komponent
+
+
+        // <section>
+        //     <p>
+        //     </p>
+        // </section>
     }
     return (
-        <chatSetup />
+        <div>
+            <Rooms />
+        </div>
     )
 }
+
+export default Rooms;
